43f7526163a005dfbfe83e9bc3b4da8d1dbd9152
Thomas Bance-Laupenie <thomas170303@gmail.com>
2023-01-18 09:43:33 +0100

    avancement tp collection

--- PileCollection/Program.cs
+++ PileCollection/Program.cs
@@ -29,7 +29,50 @@ namespace PileCollection
         /// <param name="PNbElemt">Nombre d'élément maxi de la Pile</param>
         static void InitPile(ref Pile pUnePile, int PNbElemt)
         {
+            pUnePile.maxElt = PNbElemt;
+            pUnePile.tabElem = new ArrayList ();
+        }
 
+        ///<summary>
+        ///retourne un booléen indiquant si la pile est vide
+        ///Une pile est vide si maxElt = 0
+        /// </summary>
+        /// <param name="pUnePile"></param>
+        /// <returns></returns>
+        static bool PileVide(Pile pUnePile)
+        {
+            return pUnePile.maxElt == 0;
+        }
+
+        ///<summary>
+        ///retourne un booléen indiquant si la pile est pleine
+        ///Une pile est pleine si maxElt = pUnePile.tabElem.Count
+        /// </summary>
+        /// <param name="pUnePile"></param>
+        /// <returns></returns>
+        static bool PilePleine(Pile pUnePile)
+        {
+            return pUnePile.maxElt == pUnePile.tabElem.Count;
+        }
+
+        ///<summary>
+        ///Cette méthode ajoute la valeur passée en paramètre au sommet de la pile
+        ///SI la pile n'est pas pleine.
+        ///Si la pile est pleine, déclenchement d'une exception
+        /// </summary>
+        /// <param name="pUnePile">pile sur laquelle il faut empiler</param>
+        /// <param name="PObj">éléments à empiler</param>
+
+        static void Empiler(ref Pile pUnePile, Object PObj)
+        {
+            try
+            {
+
+            }
+            catch
+            {
+
+            }
         }
     }
 }
